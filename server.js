fd93fd4b7a887fb5e9c93071aa6dfd24f811d035
MariaFandeeva <mfandeeva@gmail.com>
2023-02-27 20:25:28 +0300

    port fix

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST } = require("./config.js");
 
-mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", false);
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(PORT))
+  .then(() => {
+    app.listen(3001);
+  })
   .then(console.log("Database connection succsess"))
   .catch((error) => {
     console.log(error.message);
